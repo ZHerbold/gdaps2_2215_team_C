fb5947318694db0f0337e352d6f90b694c2643fb
Zander Herbold <zander.herbold@gmail.com>
2022-02-16 18:01:02 -0500

    Starting code

---
+++ game/TheGame/TheGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TheGame
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
